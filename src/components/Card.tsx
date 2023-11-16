19faa4290361706f71068e405791c9ea134d93bd
zapodask <joaopedroemerim@gmail.com>
2023-11-15 23:27:29 -0300

    feat: card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,20 @@
-interface ICard {
-  id: number,
-  paragraph: string,
-  details: string
-}
+import { Box, Center, Input } from "@chakra-ui/react"
+import { Button } from "./Button"
+import { login } from "../services/login"
 
-export const Card = ({ id, paragraph, details }: ICard) => {
+export const Card = () => {
   return(
-    <div>
-      <h1>Card {id}</h1>
-      <p>{paragraph}</p>
-      <p>{details}</p>
-    </div>
+    <Box minHeight='100vh' backgroundColor='#9413dc' padding='25px'>
+      <Box backgroundColor='#FFFFFF' borderRadius='25px' padding='15px' >
+        <Center>
+          <h1>Faça o login</h1>
+        </Center>
+        <Input placeholder="email" />
+        <Input placeholder="password" />
+        <Center>
+          <Button onClick={login} text='Login' />
+        </Center>
+      </Box>
+    </Box>
   )
 }
